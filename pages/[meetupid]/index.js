ea0146cc5a231bdf4146ffc98930e357e689c30e
Steve Tyler <steve.tyler75@gmail.com>
2026-02-03 23:29:39 +0000

    section 497

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -11,4 +11,42 @@ function meetingDetails() {
   );
 }
 
+// needed for dynamic pages that use getStaticProps to pre-render pages at build time
+export async function getStaticPaths() {
+  return {
+    fallback: false, // why fallback is false: if user tries to access a page that was not pre-generated, they will get 404
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: 'A First Meetup',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some Address 5, 12345 Some City',
+        description: 'This is a first meetup!',
+      },
+    },
+  };
+}
+
 export default meetingDetails;
